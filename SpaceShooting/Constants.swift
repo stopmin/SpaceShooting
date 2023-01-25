2e12ca86e6e6561b16de2012d6507520b8e5bf9b
stop._.bmin <geemin0@naver.com>
2023-01-25 17:21:20 +0900

    움직이는 별무리 배치하기

---
+++ SpaceShooting/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SpaceShooting
+//
+//  Created by 정지민 on 2023/01/25.
+//
+
+import SpriteKit
+
+struct  Particle {
+    static let starfield = "starfield"
+}
+
+struct Layer {
+    static let starfield:  CGFloat = 0
+}
--- SpaceShooting/GameScene.swift
+++ SpaceShooting/GameScene.swift
@@ -11,6 +11,11 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {    // 화면 초기화
-        
+        // 배경용 별무리 붙이기
+        guard let starfield = SKEmitterNode(fileNamed: Particle.starfield) else {return}   // 파일이 없을 경우 return
+        starfield.position = CGPoint(x: size.width / 2, y: size.height) // 화면의 중간지점, 제일 위쪽
+        starfield.zPosition = Layer.starfield
+        starfield.advanceSimulationTime(30) // 화면이 30초 진행된 상태에서 시작해라
+        self.addChild(starfield)
     }
 }
